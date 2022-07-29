b1a8764e8b110e86eaccdb73d8d0daba58c9592c
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-07-29 17:30:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const navbar= document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll('section');
-const navbarLinks = document.querySelectorAll('.navbar-link')
+const navbarLinks = document.querySelectorAll('.navbar-link');
 
+/*  Global window object */
+/*  pageYOffset - returns the distance in pixels that is being scrolled
+    offsetTop - returns the distance from the top edge of page to the element
+    Once they are equal it means the navbar has reached the top of the page
+*/
 window.addEventListener("scroll", () => {
   if (window.pageYOffset >= navbar.offsetTop) {
     navbar.classList.add("sticky");
@@ -10,4 +15,15 @@ window.addEventListener("scroll", () => {
   else {
     navbar.classList.remove("sticky");
     }
+  
+  sections.forEach(section => {
+    if(window.pageYOffset >= section.
+    offsetTop - 10) {
+      navbarLInks.forEach(navbarLink => {
+        navbarLink.classList.remove('change');
+      }
+      navbarLinks[i].classList.add('change');
+      
+  }  
+  
 });  
