e63a2093dcc220e71aca9c2050e92406ab4aae6c
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-08-07 15:36:33 -0400

    c

--- script.js
+++ script.js
@@ -11,40 +11,39 @@ const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
-do {
 
-  window.addEventListener("scroll", () => {
-    mainFn();
-  });
-
-  const mainFn = () => {
-    if (window.pageYOffset >= navbarOffsetTop) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
-
-    sections.forEach((section, i) => {
-      if (window.pageYOffset >= section.offsetTop - 10)  {
-        navbarLinks.forEach((navbarLink) => {
-          navbarLink.classList.remove("change");
-        });
-        navbarLinks[i].classList.add("change");
-      }
-    });
-
-    if (window.pageYOffset + window.innerHeight >= progress.offsetTop)  {
-      document.querySelectorAll(".progress-percent").forEach((el, i) => {
-        el.style.width = `${progressBarPercents[i]}%`;
-        el.previousElementSibling.firstElementChild.textContent =
-          progressBarPercents[i];
-      });
-    }
-  };
-
+window.addEventListener("scroll", () => {
   mainFn();
+});
 
-  window.addEventListener("resize", () => {
-    window.location.reload();
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10)  {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
   });
-} while (checkh === false)
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop)  {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+
