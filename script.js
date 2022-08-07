417a575cb8460ca7b7e2e5a1dbe3b9f5bfc23675
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-08-07 15:45:36 -0400

    c

--- script.js
+++ script.js
@@ -13,43 +13,40 @@ const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
 
 if (checkh) {
-  console.log(true);
-} else {
-  console.log(false);
-}
 
-window.addEventListener("scroll", () => {
-  mainFn();
-});
-
-const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-
-  sections.forEach((section, i) => {
-    if (window.pageYOffset >= section.offsetTop - 10)  {
-      navbarLinks.forEach((navbarLink) => {
-        navbarLink.classList.remove("change");
-      });
-      navbarLinks[i].classList.add("change");
-    }
+  window.addEventListener("scroll", () => {
+    mainFn();
   });
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop)  {
-    document.querySelectorAll(".progress-percent").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
+  const mainFn = () => {
+    if (window.pageYOffset >= navbarOffsetTop) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+
+    sections.forEach((section, i) => {
+      if (window.pageYOffset >= section.offsetTop - 10)  {
+        navbarLinks.forEach((navbarLink) => {
+          navbarLink.classList.remove("change");
+        });
+        navbarLinks[i].classList.add("change");
+      }
     });
-  }
-};
 
-mainFn();
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop)  {
+      document.querySelectorAll(".progress-percent").forEach((el, i) => {
+        el.style.width = `${progressBarPercents[i]}%`;
+        el.previousElementSibling.firstElementChild.textContent =
+          progressBarPercents[i];
+      });
+    }
+  };
 
-window.addEventListener("resize", () => {
-  window.location.reload();
-});
+  mainFn();
 
+  window.addEventListener("resize", () => {
+    window.location.reload();
+  });
+
+}
