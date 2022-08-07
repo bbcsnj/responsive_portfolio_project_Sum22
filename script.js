69e184ef2e3d18e1591f6fd1021a01d4fa278ff0
bbcsnj <71155260+bbcsnj@users.noreply.github.com>
2022-08-07 15:23:05 -0400

    c

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
-do {}
+do {
 
   window.addEventListener("scroll", () => {
     mainFn();
